09409e7229c46415f850615afb320898817397aa
Kirill Vorfolomeev <cyrill.vorfolomeev@yandex.ru>
2022-11-01 17:32:57 +0500

    fix

--- .github/scripts/fill-realese-ticket.sh
+++ .github/scripts/fill-realese-ticket.sh
@@ -11,6 +11,8 @@ echo "Поиск последних релизных тегов"
 echo "$(git tag --sort=committerdate)"
 lastTag="$(git tag --sort=committerdate | grep -E 'rc-0.0.[0-9]+' | tail -1)"
 previousTag="$(git tag --sort=committerdate | grep -E 'rc-0.0.[0-9]+' | tail -2)"
+echo "${previousTag} ${lastTag}"
+echo "$(git log ${lastTag})"
 
 echo "Создание тела для запроса заполнения релизного тикета"
 commits="$(git log ${previousTag}..${lastTag} --pretty=format:"%h | %an | %s")"
