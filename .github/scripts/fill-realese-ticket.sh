f85cf81bb1518ddc078e2aa8dd58fddee3d5112b
Kirill Vorfolomeev <cyrill.vorfolomeev@yandex.ru>
2022-11-01 16:54:36 +0500

    fix

--- .github/scripts/fill-realese-ticket.sh
+++ .github/scripts/fill-realese-ticket.sh
@@ -7,6 +7,8 @@ headerAuthorization="Authorization: OAuth ${AUTH_TOKEN}"
 headerXOrgId="X-Org-ID: ${X_ORG_ID}"
 
 echo "Поиск последних релизных тегов"
+
+echo "$(git tag --sort=committerdate)"
 lastTag="$(git tag --sort=committerdate | grep -E 'rc-0.0.[0-9]+' | tail -1)"
 previousTag="$(git tag --sort=committerdate | grep -E 'rc-0.0.[0-9]+' | tail -2)"
 
